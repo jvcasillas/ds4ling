263e6b041ffecae1990f3610a4fc51c860d72f87
jvcasillas <joseph.casillas@rutgers.edu>
2021-01-13 23:49:45 -0500

    recode ggqqplot

--- R/gg_qqplot.R
+++ R/gg_qqplot.R
@@ -7,7 +7,8 @@
 #' @keywords plot
 #' @import ggplot2
 #' @import dplyr
-#' @importFrom stats quantile qnorm cor qqnorm
+#' @import tibble
+#' @importFrom stats cor qqnorm
 #' @importFrom rlang .data
 #' @export
 #' @examples
@@ -16,27 +17,16 @@
 # QQ plot function
 gg_qqplot <- function(vec) {
 
-  # Get slope and intercept for qqline
-  # from base R's qqline()
-  y <- quantile(vec[!is.na(vec)], c(0.25, 0.75))
-  x <- qnorm(c(0.25, 0.75))
-  slope <- diff(y) / diff(x)
-  int <- y[1L] - slope * x[1L]
+the_cor <- round(cor(vec, qqnorm(vec)$x), 3)
 
-  # Calculate vec and qq correlation
-  the_cor <- cor(vec, qqnorm(vec)$x)
-
-  # Create plot
-  tibble(resid = vec) %>%
-  ggplot() +
-    aes(sample = .data$resid) +
-    geom_point(stat = 'qq', shape = 21, color = "grey30",
-               fill = 'lightblue') +
-    geom_abline(slope = slope, intercept = int) +
-    annotate("text", x = -1, y = max(y),
-             label = paste0("r = ", round(the_cor, 4))) +
-    ds4ling_theme() +
-    labs(x = "Theoretical quantiles", y = "Sample quantiles") +
-    theme(axis.line = element_line(colour = "black"))
+ggplot(tibble(vec), aes(sample = vec)) +
+  stat_qq(pch = 21, size = 1.25, fill = "grey") +
+  stat_qq_line() +
+  annotate("text", x = -1, y = mean(vec),
+    label = paste0("r = ", the_cor)) +
+  labs(x = "Theoretical quantiles", y = "Sample quantiles") +
+  ds4ling_theme() +
+  theme(axis.line = element_line(colour = "black"))
 
 }
+
